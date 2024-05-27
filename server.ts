097b75773e1a052ab60ba21adaf04949897bfa3c
fahad <fahadfaruqi1@gmail.com>
2024-05-27 16:04:44 -0400

    updating server with basic endpoint

--- server.ts
+++ server.ts
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("BYTE @ CCNY").status(200);
+})
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
+
+export default app;
