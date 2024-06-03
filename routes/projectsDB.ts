c34fed03f314777507d4e8aaf43c4b8f01896f6d
Abrar Habib <abrarhabib285@gmail.com>
2024-06-03 12:55:07 -0400

    made router constant

--- routes/projectsDB.ts
+++ routes/projectsDB.ts
@@ -1,17 +1,18 @@
 import { Client } from "pg";
 import activateDb from "../db";
 import express from "express";
-let router = express.Router();
+const router = express.Router();
+
 let client: Client | undefined;
 
 router.get("/", async (req: any, res: any) => {
     try {
         client = await activateDb();
         await client.end();
+        return res.status(200).send("No errors!!");
     } catch (err: any) {
         return res.status(500).send(err.message);
     }
-    return res.status(200).send("Projects Database");
 });
 
 export default router;
