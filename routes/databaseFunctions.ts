282b8f5ef16b1d64b139fac99bc955973a4b7705
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-06-04 12:32:39 -0400

    formatting

--- routes/databaseFunctions.ts
+++ routes/databaseFunctions.ts
@@ -1,11 +1,21 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 // todo: wrap everything in a try catch block and pass the exception to the user
 export function addToDB(client: Client, valuesToQuery: any) {
-    const query = `
+  const query = `
         INSERT INTO projects (name, "short-desc", "long-desc", team, link, image, "tech-stack", cohort, topic)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`;
 
-    const values = [valuesToQuery.name, valuesToQuery.shortDescription, valuesToQuery.longDescription, valuesToQuery.team, valuesToQuery.link, valuesToQuery.image, valuesToQuery.techStack, valuesToQuery.cohort, valuesToQuery.topic];
-    client.query(query, values);
+  const values = [
+    valuesToQuery.name,
+    valuesToQuery.shortDescription,
+    valuesToQuery.longDescription,
+    valuesToQuery.team,
+    valuesToQuery.link,
+    valuesToQuery.image,
+    valuesToQuery.techStack,
+    valuesToQuery.cohort,
+    valuesToQuery.topic,
+  ];
+  client.query(query, values);
 }
