7cb85baf537e6aa4262c49cbe5ca6e987239ab99
Ivan Chen <Ivanchen498@gmail.com>
2024-06-06 14:12:57 -0400

    Validate middleware before inserting new data into DB

---
+++ middlewares/connectDB.ts
@@ -0,0 +1,14 @@
+import getDB from "../db";
+import logger from "../utils/logger";
+
+const connectDB = async (req: any, res: any, next: any) => {
+    try {
+        req.client = await getDB();
+        next(); 
+    } catch (err: any) {
+        logger.info(err.message);
+        next("route");
+    }
+}
+
+export default connectDB;
