b720dc03919bfef224df8fe62be3e6e3ff6b177e
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-06-25 22:47:22 -0400

    adding timeout to db config

--- database/db.config.ts
+++ database/db.config.ts
@@ -1,6 +1,11 @@
 import dotenv from "dotenv";
 import { Client } from "pg";
+import logger from "utils/logger";
 dotenv.config();
+
+const secondsToMs = (seconds: number): number =>  {
+    return seconds * 1000;
+}
 
 const client = new Client({
     host: process.env.POSTGRESQL_DB_HOST,
@@ -10,11 +15,22 @@ const client = new Client({
     port: process.env.POSTGRESQL_DB_PORT
         ? parseInt(process.env.POSTGRESQL_DB_PORT)
         : 5432,
+    statement_timeout: secondsToMs(5), 
+    query_timeout: secondsToMs(5),
+    application_name: "BYTE Server",
+    connectionTimeoutMillis: secondsToMs(5),
+    idle_in_transaction_session_timeout: secondsToMs(5)
 });
 
 client.on("end", () => console.log("Client has disconnected"));
 client.on("error", (err) =>
     console.error("Unexpected error on idle client", err),
 );
+client.on('notification', (msg) => {
+  logger.alert(msg.channel)
+  logger.alert(msg.payload) 
+});
+client.on('notice', (msg) => logger.warn('notice:', msg))
+
 
 export default client;
