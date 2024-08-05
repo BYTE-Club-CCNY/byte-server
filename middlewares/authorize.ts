11fd3b3cc1e9547232f64826d6e3175ba7d10d3a
LordFarquaadtheCreator <fahadfaruqi1@gmail.com>
2024-08-05 15:17:16 -0400

    adding everything to try catch in auth

--- middlewares/authorize.ts
+++ middlewares/authorize.ts
@@ -2,18 +2,18 @@ import { queryDatabase } from "routes/databaseFunctions";
 
 const authorize = function () {
     return async (req: any, res: any, next: any) => {
-        const name = req.headers.name.toLowerCase();
-        const key = req.headers.authorization?.split(" ")[1];
-        const query = {
-            text: "SELECT * FROM apikey WHERE name = $1 AND apikey = $2",
-            values: [name, key]
-        }
-
-        if (!name || !key) {
-            return res.status(400).json({ message: "Please enter your name and key before accessing the database!" });
-        }
-
         try {
+            const name = req.headers.name.toLowerCase();
+            const key = req.headers.authorization?.split(" ")[1];
+            const query = {
+                text: "SELECT * FROM apikey WHERE name = $1 AND apikey = $2",
+                values: [name, key]
+            }
+
+            if (!name || !key) {
+                return res.status(400).json({ message: "Please enter your name and key before accessing the database!" });
+            }
+
             const result = await queryDatabase(req.client, query.text, query.values);
             if (result.rows.length === 0) {
                 return res.status(401).json({ message: "Invalid name or key!" });
