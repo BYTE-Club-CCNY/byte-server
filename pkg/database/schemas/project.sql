abddbf3f0d007c4965b8b8bdb7bf799988d757fd
Fahad Faruqi <fahadfaruqi1@gmail.com>
2025-01-12 13:58:04 -0500

    modifying the schema of project.sql to make long_desc even longer

--- pkg/database/schemas/project.sql
+++ pkg/database/schemas/project.sql
@@ -4,7 +4,7 @@ create table if not exists projects.project (
 	id UUID PRIMARY KEY NOT NULL,
 	name varchar(255),
 	short_desc varchar(255),
-	long_desc varchar(255),
+	long_desc varchar(1000),
 	link varchar(255),
 	image varchar(255),
 	tech_stack text[],
