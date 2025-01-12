bbab27283515c7fc23617e1d0cf2f8e9ae66d3c3
Fahad Faruqi <fahadfaruqi1@gmail.com>
2025-01-12 13:23:31 -0500

    adding teams relation

---
+++ pkg/database/schemas/teams.sql
@@ -0,0 +1,9 @@
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+create table if not exists projects.teams(
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    member1 UUID not null,
+    member2 UUID,
+    member3 UUID,
+    member4 UUID
+);
