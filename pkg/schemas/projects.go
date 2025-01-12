56a37a918a4a6d5b31fc34436f440e34eaf247fd
Fahad Faruqi <fahadfaruqi1@gmail.com>
2025-01-12 14:06:18 -0500

    updating and adding gorm files for all new tables

--- pkg/schemas/projects.go
+++ pkg/schemas/projects.go
@@ -1,22 +1,22 @@
 package schema
 
 import (
+	"github.com/google/uuid"
 	"github.com/lib/pq"
 )
 
 type Project struct {
-	Uid       int            `gorm:"column:uid;primaryKey;autoIncrement"`
-	Name      string         `gorm:"column:name;type:varchar;not null"`
-	ShortDesc string         `gorm:"column:short-desc;type:varchar;not null"`
-	LongDesc  string         `gorm:"column:long-desc;type:varchar"`
-	Team      pq.StringArray `gorm:"column:team;type:text[]"`
-	Link      string         `gorm:"column:link;type:varchar"`
-	Image     string         `gorm:"column:image;type:varchar"`
-	TechStack pq.StringArray `gorm:"column:tech-stack;type:text[]"`
-	Cohort    string         `gorm:"column:cohort;type:varchar"`
-	Topic     pq.StringArray `gorm:"column:topic;type:text[]"`
+	ID         uuid.UUID      `gorm:"column:id;type:uuid;primaryKey"`
+	Name       string         `gorm:"column:name;type:varchar(255)"`
+	ShortDesc  string         `gorm:"column:short_desc;type:varchar(255)"`
+	LongDesc   string         `gorm:"column:long_desc;type:varchar(1000)"`
+	Link       string         `gorm:"column:link;type:varchar(255)"`
+	Image      string         `gorm:"column:image;type:varchar(255)"`
+	TechStack  pq.StringArray `gorm:"column:tech_stack;type:text[]"`
+	Topic      pq.StringArray `gorm:"column:topic;type:text[]"`
+	CohortID   int            `gorm:"column:cohort_id;not null"`
 }
 
 func (Project) TableName() string {
-	return "public.projects"
+	return "projects.project"
 }
---
+++ pkg/schemas/teams.go
@@ -0,0 +1,17 @@
+package schema
+
+import (
+	"github.com/google/uuid"
+)
+
+type Team struct {
+	ID      uuid.UUID `gorm:"column:id;type:uuid;default:uuid_generate_v4();primaryKey"`
+	Member1 uuid.UUID `gorm:"column:member1;type:uuid;not null"`
+	Member2 *uuid.UUID `gorm:"column:member2;type:uuid"`
+	Member3 *uuid.UUID `gorm:"column:member3;type:uuid"`
+	Member4 *uuid.UUID `gorm:"column:member4;type:uuid"`
+}
+
+func (Team) TableName() string {
+	return "projects.teams"
+}
