f4d164af41845273695b589f263721f39a6a0bdc
LordFarquaadtheCreator <fahadfaruqi1@gmail.com>
2025-01-15 19:01:20 -0500

    refactored to use gorm to construct query rather than raw sql

--- pkg/database/getProjects.go
+++ pkg/database/getProjects.go
@@ -20,20 +20,3 @@ type GetProjects struct {
 	Topic			pq.StringArray	`gorm:"column:topic;type:text[]"`
 	Cohort			string			`gorm:"column:cohort;type:varchar(255)"`
 }
-
-var GetProjectsQuery string = 
-"SELECT p.id, p.name, p.topic, " +
-"p.name, p.short_desc, p.long_desc, " + 
-"p.link, p.image, p.tech_stack, " +
-"CONCAT(u1.first_name, ' ', u1.middle_name, ' ', u1.last_name) AS member1, " +
-"CONCAT(u2.first_name, ' ', u2.middle_name, ' ', u2.last_name) AS member2, " +
-"CONCAT(u3.first_name, ' ', u3.middle_name, ' ', u3.last_name) AS member3, " +
-"CONCAT(u4.first_name, ' ', u4.middle_name, ' ', u4.last_name) AS member4, " +
-"c.cohort_name as cohort " +
-"FROM projects.project p " +
-"INNER JOIN projects.teams t ON p.id = t.id " +
-"LEFT JOIN users.people u1 ON t.member1 = u1.uid " +
-"LEFT JOIN users.people u2 ON t.member2 = u2.uid " +
-"LEFT JOIN users.people u3 ON t.member3 = u3.uid " +
-"LEFT JOIN users.people u4 ON t.member4 = u4.uid " +
-"INNER JOIN users.cohort c ON p.cohort_id = c.cohort_id;"
--- pkg/projects/projects.go
+++ pkg/projects/projects.go
@@ -60,19 +60,34 @@ func get(c *fiber.Ctx) error {
 	var projects []database.GetProjects
 	query := database.DB.Limit(params.N_records)
 
-	err := query.Raw(database.GetProjectsQuery).Scan(&projects).Error
-	// if params.Cohort != -1 {
-	// 	query = query.Where("cohort_id = ?", params.Cohort)
-	// }
-	// if params.Name != "" {
-	// 	query = query.Where("name = ?", params.Name)
-	// }
 
-	// err := query.Find(&projects)
+	query.Table("projects.project AS p").
+		Select(`p.id, p.name, p.topic, p.short_desc, p.long_desc, 
+				p.link, p.image, p.tech_stack,
+				CONCAT(u1.first_name, ' ', u1.middle_name, ' ', u1.last_name) AS member1,
+				CONCAT(u2.first_name, ' ', u2.middle_name, ' ', u2.last_name) AS member2,
+				CONCAT(u3.first_name, ' ', u3.middle_name, ' ', u3.last_name) AS member3,
+				CONCAT(u4.first_name, ' ', u4.middle_name, ' ', u4.last_name) AS member4,
+				c.cohort_name AS cohort`).
+		Joins("INNER JOIN projects.teams t ON p.id = t.id").
+		Joins("LEFT JOIN users.people u1 ON t.member1 = u1.uid").
+		Joins("LEFT JOIN users.people u2 ON t.member2 = u2.uid").
+		Joins("LEFT JOIN users.people u3 ON t.member3 = u3.uid").
+		Joins("LEFT JOIN users.people u4 ON t.member4 = u4.uid").
+		Joins("INNER JOIN users.cohort c ON p.cohort_id = c.cohort_id")
 
-	if err != nil {
+	if params.Cohort != -1 {
+		query = query.Where("p.cohort_id = ?", params.Cohort)
+	}
+	if params.Name != "" {
+		query = query.Where("p.name = ?", params.Name)
+	}
+
+	query.Scan(&projects)
+
+	if query.Error != nil {
 		return c.Status(500).JSON(fiber.Map{
-			"message": err.Error(),
+			"message": query.Error.Error(),
 		})
 	}
 
