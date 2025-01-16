e123011dc6f24fbd7598a81b76476d865a4bfb13
Fahad Faruqi <fahadfaruqi1@gmail.com>
2025-01-16 01:04:24 -0500

    adding cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"byteserver/pkg/projects"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {	
@@ -12,6 +13,11 @@ func main() {
 	app := fiber.New()
 	database.InitDB()
 	
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "https://www.byteccny.com, http://www.byteccny.com, http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("BYTE Server is running!")
 	})
