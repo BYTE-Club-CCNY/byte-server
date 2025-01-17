ee8dba091158458c0245b1420ab2f90528aee878
IshmamF <ishmamf2003@gmail.com>
2025-01-17 16:29:55 -0500

    better error handling / messages

--- pkg/apps/structs.go
+++ pkg/apps/structs.go
@@ -4,3 +4,7 @@ type EditCohortDraftBody struct {
 	ProjectID		string		`json:"project_id" form:"project_id" validate:"required"`
 	ProjectSchema 	string		`json:"project_schema" form:"project_schema" validate:"required"`
 }
+
+type Collection struct {
+	Name string `json:"collection" form:"collection" validate:"required"`
+}
--- pkg/mongo/functions.go
+++ pkg/mongo/functions.go
@@ -2,33 +2,39 @@ package mongodb
 
 import (
 	"context"
+	"errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"log"
+	"fmt"
 )
 
-func CreateNewCollection(season *string) error {
-	err := db.CreateCollection(context.TODO(), *season) 
-	if err != nil {
-		return err
+func CreateNewCollection(ctx context.Context, collection string) error {
+	if collection == "" {
+		return errors.New("collection name cannot be empty")
 	}
+
+	if err := db.CreateCollection(ctx, collection); err != nil {
+		log.Printf("failed to create collection '%s': %w", collection, err)
+		return fmt.Errorf("failed to create collection '%s': %w", collection, err)
+	}
+	
 	return nil
 }
 
-func GetAllApps(season string) []bson.M {
+func GetAllData(ctx context.Context, season string) ([]bson.M, error) {
 	collection := db.Collection(season)
 	var result []bson.M
-	cursor, err := collection.Find(context.TODO(), bson.D{})
+	cursor, err := collection.Find(ctx, bson.D{})
 	if err != nil {
-		log.Println("Failed to retrieve applications from collection")
-		panic(err)
+		log.Printf("Failed to retrieve applications from collection '%s': %v", season, err)
+		return nil, err
 	}
-	defer cursor.Close(context.TODO())
+	defer cursor.Close(ctx)
 
-	if err = cursor.All(context.TODO(), &result); err != nil {
-		log.Println("Failed to decode all documents")
-		panic(err)
+	if err = cursor.All(ctx, &result); err != nil {
+		log.Printf("Failed to decode documents from collection '%s': %v", season, err)
+		return nil, err
 	}
 
-
-	return result
+	return result, nil
 }
