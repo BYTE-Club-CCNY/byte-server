97c7fdd93720c7e88fc4a126dac2c1d49bce00a2
IshmamF <ishmamf2003@gmail.com>
2025-01-16 20:36:54 -0500

    script to add all people from csv to database

---
+++ scripts/migrate-people/add-people.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"encoding/csv"
+	schema "byteserver/pkg/schemas"
+	"byteserver/pkg/database"
+	"byteserver/pkg/utils"
+)
+
+func main () {
+	utils.IshmamLoadEnv()
+	database.InitDB()
+	fmt.Println("Database connection:", database.DB)
+	file, err := os.Open("./scripts/migrate-people/data.csv")
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+    defer file.Close()
+
+    reader := csv.NewReader(file)
+
+    records, err := reader.ReadAll()
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+
+	for _, record := range records {
+		user := new(schema.User)
+		user.PersonalEmail = record[0]
+		user.FirstName = record[1]
+		if record[2] != "" {
+			user.MiddleName = record[2]
+		}
+		user.LastName = record[3]
+		user.CunyEmail = record[4]
+		user.Emplid = record[5]
+		user.Discord = record[6]
+		fmt.Println(user)
+
+		res := database.DB.Create(&user)
+
+		if res.Error != nil{
+			fmt.Println("adding user error for: ", record)
+		}
+    }
+	fmt.Println("Successfully added all people")
+}
---
+++ scripts/migrate-people/readme.md
@@ -0,0 +1,8 @@
+## Description
+> Script go through the csv and add the people to database. Just make sure to remove the data after it's added. 
+
+## Structure of each row in csv
+> Each value represents the column. Don't include the column names.
+```
+email, first_name, middle_name, last_name, citymail, emplid, discord
+```
