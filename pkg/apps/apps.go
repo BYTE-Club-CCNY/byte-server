72c55a253f0c04bf6f3a7d5dae433662094c1c97
IshmamF <ishmamf2003@gmail.com>
2025-01-19 18:58:24 -0500

    fixed dumb mistake where i did /apps/view-draft instead of /view-draft

--- pkg/apps/apps.go
+++ pkg/apps/apps.go
@@ -11,8 +11,10 @@ func App() *fiber.App {
 	app.Post("/new-cohort", NewCohort) // creates a new collection
 	app.Post("/edit-draft", EditCohortDraft) // edit a cohort draft
 
-	app.Get("/apps/view-draft", ViewDraft) // view draft
-	app.Post("/apps/create-draft", CreateDraft) // create a new draft
+	app.Get("/view-draft", ViewDraft)
+	
+	app.Post("/create-draft", CreateDraft) // create a new draft
+
 	
 	/*
 	create cohort, cohort id, application layout, set deadline
@@ -23,16 +25,16 @@ func App() *fiber.App {
 	/*
 	app.Post("publish-draft")
 
-	app.Get("/apps/get-template") // question layout per cohort (mongo)*/
+	app.Get("/get-template") // question layout per cohort (mongo)*/
 
 	/* saves application w/ userid (uuid same as the one in postgres) 
 	to mongo application collection */
 	/*
-	app.Post("/apps/submit-app") 
+	app.Post("/submit-app") 
 
-	app.Post("/apps/save-app-draft") // save draft only if app is not submitted
+	app.Post("/save-app-draft") // save draft only if app is not submitted
 
-	app.Get("/apps/view") // get existing application(s) query by id and page
+	app.Get("/view") // get existing application(s) query by id and page
 	*/
 
 	return app
