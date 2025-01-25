72843851886bfda2e773305be2b10a1631f3137c
IshmamF <ishmamf2003@gmail.com>
2025-01-24 19:53:11 -0500

    moved struct to utils to access across packages

--- pkg/utils/structs.go
+++ pkg/utils/structs.go
@@ -7,3 +7,15 @@ type AddUsersBody struct{
 	PersonalEmail	string		`json:"personal_email" form:"personal_email"`
 	Discord			string		`json:"discord" form:"discord"`
 }
+
+type EditDraft struct {
+	Cohort_id string `json:"cohort_id" form:"cohort_id" validate:"required"`
+	Deadline string `json:"deadline" form:"deadline" validate:"required"`
+	Questions []Question `json:"questions" form:"questions" validate:"required"` 
+}
+
+type Question struct {
+	Question   string   `json:"question"`
+	AnswerType string   `json:"answerType"`
+	Options    []string `json:"options"`
+}
