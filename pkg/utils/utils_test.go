a08e6768489dfbf4889fd99a8a052ccb258da807
Fahad Faruqi <fahadfaruqi1@gmail.com>
2025-01-27 12:49:55 -0500

    removing function to test if env vars exist - not relavent to testing

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -32,21 +32,6 @@ func TestInitEnv(t *testing.T) {
 	}
 }
 
-func TestEnvironmentValuesExist(t *testing.T) {
-	_ = InitEnv()
-
-	keys := [5]string{"POSTGRESQL_DB_HOST", "POSTGRESQL_DB_USER", 
-	"POSTGRESQL_DB_PASSWORD", "POSTGRESQL_DB", "POSTGRESQL_DB_PORT"};
-
-	for _, key := range keys {
-		_, error := GetEnv(key)
-
-		if error != nil {
-			panic(fmt.Sprintf("Missing %s", key));
-		}
-	}
-}
-
 func TestValidate(t *testing.T) {
 	app := fiber.New()
 	c := app.AcquireCtx(&fasthttp.RequestCtx{})
